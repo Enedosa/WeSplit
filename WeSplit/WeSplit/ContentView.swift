c3a9ddc89330fa9f4f6f8a5873bfead1737a3b96
Ebele <enedosa@yahoo.com>
2022-07-12 19:01:01 +0100

    Added a picker using ForEach to select from an array through the array element's id

--- WeSplit/WeSplit/ContentView.swift
+++ WeSplit/WeSplit/ContentView.swift
@@ -9,20 +9,40 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var tapCount = 0
+    @State private var name = ""
+    let students = ["He", "She", "It"]
+    @State private var selectedStudent = "Ha"
+    
+    
     var body: some View {
         NavigationView {
             Form {
+                Picker("Select your student", selection: $selectedStudent) {
+                    ForEach(students, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
                 Section {
                     Text("Hello, world!")
                     Text("Hello, world!")
                     Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
-                    Text("Hello, world!")
+                   
                 }
                 Group {
                     Button("Tap count: \(tapCount)") {
                         tapCount += 1
+                    }
+                    TextField("Enter your name", text: $name)
+                    Text("Your name is: \(name)")
+                }
+                Section {
+                    ForEach(0..<2) {
+                        number in Text("Row \(number + 1)")
+                    }
+                    //or
+                    ForEach(0..<3) {
+                        Text("Line \($0 + 1)")
                     }
                 }
     //            Text("Hello, world!")
